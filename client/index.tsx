13e75837ed23a75d8b330f0be72820f5fc73a2c7
Antoine Quinquenel <aquinq@hotmail.fr>
2019-07-12 01:48:36 +0200

    add default styles and appropriate (s)css loaders. Update webpack config for hot module replacement.

--- client/index.tsx
+++ client/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./components/App";
 import { hot } from "react-hot-loader/root";
+import "./styles/main.scss";
 
 const app = () => <App />;
 const root = document.getElementById("react-app-root-node");
